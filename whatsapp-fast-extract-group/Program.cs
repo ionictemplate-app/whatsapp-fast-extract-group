e0dde66ce3fd4ca4b57acbb4f067318fac5a6e44
ionictemplate <53865250+ionictemplate-app@users.noreply.github.com>
2023-01-17 21:40:45 +0800

    Add project files.

---
+++ whatsapp-fast-extract-group/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace whatsapp_fast_extract_group
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
